6ddb4311eb2d320dab63c5cdb20d9ff797680160
Mortem <a20203607@pucp.edu.pe>
2023-09-28 20:41:48 -0500

    primer comit

---
+++ lab720203607/src/main/java/com/example/lab720203607/Lab720203607Application.java
@@ -0,0 +1,13 @@
+package com.example.lab720203607;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab720203607Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab720203607Application.class, args);
+    }
+
+}
---
+++ lab720203607/src/test/java/com/example/lab720203607/Lab720203607ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab720203607;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab720203607ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
